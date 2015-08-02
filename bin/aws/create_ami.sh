b5d976f4af26fa4a3fe1bf9fcbd925cc6a35590d
YL <YL@ThinkPad>
2015-08-02 17:49:58 +1200

    Initial - create image

--- bin/aws/create_ami.sh
+++ bin/aws/create_ami.sh
@@ -5,7 +5,10 @@
 ## Require
 ## AWS CLI - http://aws.amazon.com/cli/
 
+# $1 - AWS parameter override file
+
 . `cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`/../../lib/shared.func
+. `cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`/../../lib/aws.func $1 # AWS setup
 . `cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`/../../lib/folder.func # @ $_WORK_DIR
 
 ## Define variables
@@ -21,10 +24,15 @@ mkdir -p "$VIRTUALENV"
 ~/.local/bin/virtualenv $VIRTUALENV -q
 . $VIRTUALENV/bin/activate
 
+AWS="`which python` "${VIRTUALENV}"/bin/aws"
+
+# create configuration files
+#export AWS_CONFIG_FILE="$VIRTUALENV/aws.cfg" # override default location
+
 # install awscli in virtualenv
 pip install --upgrade awscli -q || ( console_output "ERROR" "Failed to install awscli using pip in virtualenv, existing" && deactivate && exit 1 )
 
-## TODO - ec2-create-imageec2-create-image to create EC2 instance
+$AWS ec2 create-image --instance-id i-2bc338f9 --name "Clone AMI" --description "An AMI for build automation"
 
 # deactivate virtualenv
 deactivate
